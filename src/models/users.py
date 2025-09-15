d45f00efbeda6db1be7d90c9bef2131500589953
Noobmaster005 <ayb.saidahmedov@gmail.com>
2025-09-15 14:37:29 +0300

    unique email

--- src/models/users.py
+++ src/models/users.py
@@ -3,12 +3,14 @@
 from src.database import Base
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import Integer, String
-
+import jwt
 
 class UsersOrm(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    #unique=True, позволяет сделать поле email уникальным,
+    #у нас не должно быть двух пользователей с одинаковым email
     email: Mapped[str] = mapped_column(String(100), unique=True)
     nickname: Mapped[str] = mapped_column(String(100))
     hashed_password: Mapped[str] = mapped_column(String(100))
